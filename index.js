9eaf4e270d6ea17c862175f3779902ba1fa974c0
ESWARASIDDARTH-GS <slim.siddhu@gmail.com>
2022-07-15 02:04:00 +0530

    cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
-
+app.options('*',cors())
 const server = http.createServer(app);
 
 const io = new Server(server, {
